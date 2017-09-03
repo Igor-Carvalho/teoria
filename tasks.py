ca359b2c6bc0c1619711d810fd407c070a8ba685
igor <igor.dev.py@gmail.com>
2017-09-03 12:15:08 -0300

    fix: alterado nome da tarefa de distribuição

--- tasks.py
+++ tasks.py
@@ -30,8 +30,8 @@ def run_server(ctx, settings='development', noinput=True, clear=False):
 
 
 @invoke.task
-def deploy(ctx, settings='production', noinput=True, clear=True):
-    """Comando utilizado no git hook post-merge para efetuar as configurações do projeto."""
+def distribuir(ctx, settings='production', noinput=True, clear=True):
+    """Comando utilizado no git hook post-merge para efetuar as configurações do projeto em produção."""
     ctx.run('./manage.py migrate')
     assetsbuild(ctx, settings, noinput, clear)
     ctx.run('sudo service nginx restart')
