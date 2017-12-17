b052fe49a548418431275f49c24eb51094141d05
igor <igor.dev.py@gmail.com>
2017-12-16 21:09:18 -0300

    fix: corrigidas configurações de teste

--- teoria/settings/test.py
+++ teoria/settings/test.py
@@ -4,3 +4,6 @@ from .development import *  # noqa
 
 PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
 AUTH_PASSWORD_VALIDATORS = []
+
+MIDDLEWARE = MIDDLEWARE.copy()
+MIDDLEWARE.remove('core.middleware.HttpsOnlyMiddleware')
