6127cea612e0df8c385a28b51955cf7cf7e718a9
igor <igor.dev.py@gmail.com>
2018-03-03 15:32:35 -0300

    fix: corrigido csrf token e migração

--- teoria/assets/app/app.config.js
+++ teoria/assets/app/app.config.js
@@ -1,3 +1,6 @@
 Vue.use(VueResource);
 Vue.use(VeeValidate);
 Vue.use(VToaster, {timeout: 2000});
+
+Vue.http.headers.common['xsrfCookieName'] = 'csrftoken';
+Vue.http.headers.common['xsrfHeaderName'] = 'X-CSRFToken';
