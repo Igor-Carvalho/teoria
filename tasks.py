fc0c1021d462ef7bd91758417b4243cd46a4262e
igor <igor.dev.py@gmail.com>
2018-03-03 13:52:47 -0300

    fix: task de distribuição corrigida

--- tasks.py
+++ tasks.py
@@ -33,7 +33,7 @@ def run_server(ctx, settings='development', noinput=True, clear=False):
 @invoke.task
 def distribuir(ctx, settings='production', noinput=True, clear=True):
     """Comando utilizado no git hook post-merge para efetuar as configurações do projeto em produção."""
-    ctx.run('pip3 install -r requirements.txt')
+    ctx.run('pipenv install')
     ctx.run('./manage.py migrate')
     assetsbuild(ctx, settings, noinput, clear)
     ctx.run('sudo service nginx restart')
